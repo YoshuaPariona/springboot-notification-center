10036e56030e5bb01eb233c1a73cacd2a9d15c6b
YoshuaPariona <loganpariona.1207@gmail.com>
2025-12-29 03:08:21 -0500

    feat: add telegram bot that replicates messages
    Add two dependencies for managing and registering bots, then create a class that allows the bot to replicate messages.

---
+++ src/main/java/com/bot/telegram/telegramBots/SpringAlertBot.java
@@ -0,0 +1,62 @@
+package com.bot.telegram.telegramBots;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Component
+public class SpringAlertBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+
+    private final String botToken;
+    private final TelegramClient telegramClient;
+
+    public SpringAlertBot(@Value("${telegram.bot.token}") String botToken) {
+        this.botToken = botToken;
+        this.telegramClient = new OkHttpTelegramClient(botToken);
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            SendMessage message = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text(messageText)
+                    .build();
+            try {
+                telegramClient.execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        System.out.println("El estado del bot registrado es: " + botSession.isRunning());
+    }
+}
