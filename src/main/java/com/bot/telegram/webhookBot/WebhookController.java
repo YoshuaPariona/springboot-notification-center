239e0229f295e26a5f4c171ecad523d7224e0b4b
YoshuaPariona <loganpariona.1207@gmail.com>
2026-01-03 23:30:25 -0500

    build: Added and removed dependencies for webhook

--- src/main/java/com/bot/telegram/webhookBot/WebhookController.java
+++ src/main/java/com/bot/telegram/webhookBot/WebhookController.java
@@ -1,45 +1,26 @@
 package com.bot.telegram.webhookBot;
 
-import org.apache.http.client.ResponseHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.util.WebhookUtils;
 
 
 @RestController
+@RequestMapping("/webhook")
 public class WebhookController {
 
-    @Value("${telegram.bot.token}")
-    private String botToken;
+    private final WebhookBot bot;
 
-    @Value("${telegram.bot.username}")
-    private String botUsername;
-
-    @Value("${telegram.bot.webhook-path}")
-    private String webhookPath;
-
-    @PostMapping("${telegram.bot.webhook-path")
-    public BotApiMethod<?> handleUpdate(@RequestBody Update update) {
-
-        if(update.hasMessage() && update.getMessage().hasText()) {
-            Long chatId = update.getMessage().getChatId();
-            String text = update.getMessage().getText();
-
-            SendMessage response = SendMessage.builder()
-                    .chatId(chatId)
-                    .text(text)
-                    .build();
-        }
-        return null;
+    public WebhookController(WebhookBot bot) {
+        this.bot = bot;
     }
-    
+
+    @PostMapping
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return bot.onUpdate(update);
+    }
 
 }
