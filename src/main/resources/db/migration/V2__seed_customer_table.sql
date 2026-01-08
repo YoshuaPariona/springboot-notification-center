27752f239f4c9e7f6a694b4849af55a8b65493c6
YoshuaPariona <loganpariona.1207@gmail.com>
2026-01-07 23:22:19 -0500

    feat: Set basic seed for Customer table

---
+++ src/main/resources/db/migration/V2__seed_customer_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (name, churn_value) VALUES ('Juan', 0.4);
+INSERT INTO customer (name, churn_value) VALUES ('María', 0.7);
+INSERT INTO customer (name, churn_value) VALUES ('Rodrigo', 0.8);
