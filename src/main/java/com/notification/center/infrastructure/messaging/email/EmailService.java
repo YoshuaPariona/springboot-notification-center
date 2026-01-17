82b6c7b3f2452a24a44d29c3357883ba1789c709
YoshuaPariona <loganpariona.1207@gmail.com>
2026-01-17 11:56:21 -0500

    feat: Send the email to the customer from request

--- src/main/java/com/notification/center/infrastructure/messaging/email/EmailService.java
+++ src/main/java/com/notification/center/infrastructure/messaging/email/EmailService.java
@@ -2,6 +2,7 @@ package com.notification.center.infrastructure.messaging.email;
 
 import com.notification.center.application.dto.ExternalResponse;
 import com.notification.center.domain.model.Customer;
+import com.notification.center.infrastructure.event.HighChurnEvent;
 import com.notification.center.infrastructure.event.MediumChurnEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
@@ -29,8 +30,7 @@ public class EmailService {
         String body = "Le informamos que el usuario %s ha obtenido un resultado de churn de %.2f"
                 .formatted(customer.getFullName(), exResponse.churnScore());
         message.setFrom(backEmail);
-        //A sí mismo por mientras
-        message.setTo(backEmail);
+        message.setTo(customer.getEmail());
         message.setSubject(subject);
         message.setText(body);
 
@@ -41,5 +41,10 @@ public class EmailService {
     @EventListener
     public void onMediumChurn(MediumChurnEvent mediumChurnEvent) {
         sendTextEmail(mediumChurnEvent.customer(), mediumChurnEvent.exResponse());
+    }
+
+    @EventListener
+    public void onHighChurn(HighChurnEvent highChurnEvent) {
+        sendTextEmail(highChurnEvent.customer(), highChurnEvent.exResponse());
     }
 }
